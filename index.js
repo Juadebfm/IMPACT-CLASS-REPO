c7a6740bf028ef6aab06889290ce83b93c92023b
stellanechi <stellanechi65@gmail.com>
2026-09-02 09:25:17 +0100

    Fix: I restyled the product card

---
+++ index.js
@@ -0,0 +1,4 @@
+"use strict";
+console.log("Hello, World of TypeScript!");
+let age = 25;
+let myName = "John Doe";
---
+++ index.ts
@@ -0,0 +1,4 @@
+console.log("Hello, World of TypeScript!");
+
+let age: number = 25;
+let myName: string = "John Doe";
